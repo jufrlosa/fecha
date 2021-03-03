a66b5d127afe97d1b64564dbe13d052886e990fa
Juanfri <Juanfri@DESKTOP-F1D5T59>
2021-03-03 23:17:10 +0100

    Utiliza las funciones de refactorización para sustituir el código que se encarga de validar una fecha por un método que llamarás "validar", que será público, de forma que se incluyan los comentarios para la documentación. Muestra cómo se hace en los dos entornos. (En Eclipse y en Netbeans). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio

--- src/fecha/fecha.java
+++ src/fecha/fecha.java
@@ -21,10 +21,7 @@ public class fecha {
 
 	public boolean valida() {
 
-		if (dia < 1 || dia > 31)
-			return false;
-		if (mes < 1 || mes > 12)
-			return false;
+		validar();
 
 // Determinamos la cantidad de días del mes:
 		int diasMes = 0;
@@ -58,6 +55,18 @@ public class fecha {
 		else
 			return true;
 
+	}
+
+	/**
+	 * @return 
+	 * 
+	 */
+	public boolean validar() {
+		if (dia < 1 || dia > 31)
+			return false;
+		if (mes < 1 || mes > 12)
+			return false;
+		return false;
 	}
 
 }
